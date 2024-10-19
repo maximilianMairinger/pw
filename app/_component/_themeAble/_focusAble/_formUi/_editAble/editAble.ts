6c0b5813c67d981112edce24708a288625ce8248
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-10-19 02:59:19 +0200

    editAble init state

--- app/_component/_themeAble/_focusAble/_formUi/_editAble/editAble.ts
+++ app/_component/_themeAble/_focusAble/_formUi/_editAble/editAble.ts
@@ -19,9 +19,12 @@ export default class EditAble<T = string> extends FormUi<HTMLElement> {
   public value: Data<T>
   public isEmpty: ReadonlyData<boolean>
 
-  public validate: (inp: string) => T = (a) => a as T
+
+  public validation: Data<(inp: string) => T> = new Data((a) => a as T)
   public currentErrorMsg: ReadonlyData<string | null>
   public currentlyInvalid: ReadonlyData<boolean>
+
+  private inInitState = new Data(true)
 
 
   protected placeholderContainer = ce("placeholder-container")
@@ -67,26 +70,35 @@ export default class EditAble<T = string> extends FormUi<HTMLElement> {
 
     this.value = new Data()
     const sub1 = this.value.get((v) => {
-      sub2.setToData(v + "")
+      valueSub1.deactivate()
+      valueSub2.deactivate()
+      this._value.set(v + "")
+      valueSub1.activate()
+      valueSub2.activate()
     }, false)
 
-    const showInvalidation = new Data(false)
+    const showInvalidationIfNotInInitState = new Data(false)
     currentlyInvalid.get((invalid) => {
-      if (invalid) showInvalidation.set(!currentlyWriting.get())
-      else showInvalidation.set(false)
+      if (invalid) showInvalidationIfNotInInitState.set(!currentlyWriting.get())
+      else showInvalidationIfNotInInitState.set(false)
     }, false)
     currentlyWriting.get((writing) => {
-      if (writing) showInvalidation.set(currentlyInvalid.get())
-      else showInvalidation.set(currentlyInvalid.get())
+      if (writing) showInvalidationIfNotInInitState.set(currentlyInvalid.get())
+      else showInvalidationIfNotInInitState.set(currentlyInvalid.get())
     }, false)
+    
+
+    const showInvalidation = new Data<boolean>()
+    new DataCollection(this.inInitState, showInvalidationIfNotInInitState).get((inInitState, showInvalidationIfNotInInitState) => {
+      showInvalidation.set(showInvalidationIfNotInInitState && !inInitState)
+    })
     toggleClass(this, showInvalidation, "showInvalidation")
 
     
     const currentErrorMsg = this.currentErrorMsg = new Data(null) as any
-    const sub2 = this._value.get((s) => {
-      currentlyWriting.set(true)
+    const valueSub1 = new DataCollection(this._value, this.validation, this.recalcValidData).get((s, validate) => {
       try {
-        sub1.setToData(this.validate(s))
+        sub1.setToData(validate(s))
         currentlyInvalid.set(false)
         currentErrorMsg.set(null)
       }
@@ -94,6 +106,10 @@ export default class EditAble<T = string> extends FormUi<HTMLElement> {
         currentlyInvalid.set(true)
         currentErrorMsg.set(e.message)
       }
+    }, true)
+
+    const valueSub2 = this._value.get(() => {
+      currentlyWriting.set(true)
     }, false)
 
     const tip = makeTippy(this, {
@@ -101,7 +117,7 @@ export default class EditAble<T = string> extends FormUi<HTMLElement> {
       placement: "right"
     })
 
-    currentlyInvalid.get((invalid) => {
+    showInvalidation.get((invalid) => {
       if (!invalid) tip.setProps({trigger: "manual"})
       else tip.setProps({trigger: "mouseenter focus"})
     })
@@ -136,6 +152,16 @@ export default class EditAble<T = string> extends FormUi<HTMLElement> {
       this.placeholderText.css({fontWeight: isEmpty ? "normal" : "bold"})
     })
 
+  }
+
+  private recalcValidData = new Data(false)
+  recalculateValidation() {
+    this.recalcValidData.set(!this.recalcValidData.get())
+  }
+
+  checkIfValid() {
+    this.inInitState.set(false)
+    return this.currentlyInvalid.get()
   }
   focus() {
     this.inputElem.focus()
